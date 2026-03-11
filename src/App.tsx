3b12190d765f3fb72eb442fd1d6c6ccb650748ef
Kamil Kregiel <reasonsxx@gmail.com>
2026-03-11 07:56:14 +0100

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="text-3xl font-bold">LifeOS</h1>
+        </div>
+    )
+}
+
+
+export default App
